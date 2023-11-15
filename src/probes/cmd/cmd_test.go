1bffa0e6d28725fd43aa1d0b14eb23dc3be83afc
archer-v <mandalorian-one@users.noreply.github.com>
2023-11-15 12:58:56 +0500

    Add tests to cmd that stays alive after probe finished

--- src/probes/cmd/cmd_test.go
+++ src/probes/cmd/cmd_test.go
@@ -11,47 +11,56 @@ import (
 
 func TestCmd_Runner(t *testing.T) {
 	ctx := context.Background()
-	options := model.ProbeOptions{Timeout: time.Millisecond * 1000, Expect: true}
+	defOptions := model.ProbeOptions{Timeout: time.Millisecond * 1000, Expect: true}
 
 	type testCase struct {
+		name           string
 		config         any
+		options        model.ProbeOptions
 		expectedResult bool
 		expectedError  error
 	}
 
 	cases := []testCase{
 		{
+			"command with args is started and returns true",
 			Config{Cmd: "ls", Args: []string{"-la"}, LogDump: false},
+			defOptions,
 			true,
 			nil,
 		},
 		{
+			"command with args is started and returns false",
 			Config{Cmd: "ls", Args: []string{"-unknown_flag"}, LogDump: false},
+			defOptions,
 			false,
 			nil,
 		},
 		{
-			"ls .",
-			true,
-			nil,
-		},
-		{
+			"command with described as a string is started and returns true",
 			"test/cmd_sleep.sh 0.5 0",
+			defOptions,
 			true,
 			nil,
 		},
 		{
-			Config{Cmd: "test/cmd_sleep.sh", Args: []string{"0.5", "1"}, ExitCode: 1, LogDump: false},
-			true,
-			nil,
-		},
-		{
-			Config{Cmd: "test/cmd_sleep.sh", Args: []string{"0.5", "1"}, ExitCode: 0, LogDump: false},
+			"command is started and aborted with timeout and returns false",
+			Config{Cmd: "test/cmd_sleep.sh", Args: []string{"2", "0"}},
+			defOptions,
 			false,
 			nil,
 		},
 		{
-			Config{Cmd: "test/cmd_sleep.sh", Args: []string{"2", "0"}, ExitCode: 0, LogDump: false},
+			"command is started and stays alive and probe returns true",
+			Config{Cmd: "test/cmd_sleep.sh", Args: []string{"3", "0"}},
+			model.ProbeOptions{Timeout: time.Millisecond * 200, Expect: true, StayAlive: true},
+			true,
+			nil,
+		},
+		{
+			"command is started and have to stays alive but finished and probe returns false",
+			Config{Cmd: "test/cmd_sleep.sh", Args: []string{"0.5", "0"}},
+			model.ProbeOptions{Timeout: time.Millisecond * 1000, Expect: true, StayAlive: true},
 			false,
 			nil,
 		},
@@ -63,7 +72,7 @@ func TestCmd_Runner(t *testing.T) {
 		},
 	}
 	for i, c := range cases {
-		p, err := contructor.NewProbe(options, c.config)
+		p, err := contructor.NewProbe(c.options, c.config)
 		if c.expectedError == nil && err != nil {
 			t.Errorf("Probe %v constructor returned error %v", i, err)
 			continue
