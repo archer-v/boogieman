e4fcc251337c4d2cda5d5e3b8c26ce418a4be069
archer-v <mandalorian-one@users.noreply.github.com>
2023-11-30 19:51:27 +0500

    Add Result and ResultFinish methods

--- src/model/task.go
+++ src/model/task.go
@@ -7,21 +7,17 @@ type Task struct {
 	CGroup string `json:"-"`
 	Probe  Prober
 	Worker
-	runCounter uint
 }
 
 type TaskResult struct {
-	Name       string
-	ID         uint
-	RunCounter uint
-	Result
-	Status string
-	Probe  ProbeResult
+	Name   string `json:"name"`
+	Result `json:"result"`
+	Status string      `json:"status"`
+	Probe  ProbeResult `json:"probe"`
 }
 
 func (t *Task) Start(ctx context.Context) (succ bool, err error) {
-	t.runCounter++
-	if err = t.EStatusRun(t.runCounter); err != nil {
+	if err = t.EStatusRun(); err != nil {
 		return
 	}
 	succ = t.Probe.Start(context.WithValue(ctx, "id", t.Name))
@@ -29,8 +25,21 @@ func (t *Task) Start(ctx context.Context) (succ bool, err error) {
 	return
 }
 
-func (t *Task) RunCounter() uint {
-	return t.runCounter
+func (t *Task) Result() (tr TaskResult) {
+	tr.Name = t.Name
+	trr, trs := t.Worker.Result()
+	tr.Result = trr
+	tr.Status = string(trs)
+	tr.Probe = t.Probe.Result()
+	return
+}
+
+func (t *Task) ResultFinished() (tr TaskResult) {
+	tr.Name = t.Name
+	tr.Result = t.Worker.ResultFinished()
+	tr.Status = string(EStatusFinished)
+	tr.Probe = t.Probe.ResultFinished()
+	return
 }
 
 func NewTask(taskName string, cgroup string, probe Prober) (t *Task) {
