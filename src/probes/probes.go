72bb8ab4dfb4c5395e4dc1d4d4e43700f7925e28
archer-v <mandalorian-one@users.noreply.github.com>
2023-12-08 18:49:28 +0500

    Rename openvpnConnect to openvpn

--- src/probes/probes.go
+++ src/probes/probes.go
@@ -2,7 +2,7 @@ package probes
 
 import (
 	_ "boogieman/src/probes/cmd"
-	_ "boogieman/src/probes/openvpnConnect"
+	_ "boogieman/src/probes/openvpn"
 	_ "boogieman/src/probes/ping"
 	_ "boogieman/src/probes/traceroute"
 	_ "boogieman/src/probes/web"
